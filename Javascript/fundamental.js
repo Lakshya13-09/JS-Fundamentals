c956e816cc1dc1238fefbf374aab2a310c0f7402
Lakshya13-09 <lakshyajha1309@gmail.com>
2026-03-18 12:09:00 +0530

    index file

---
+++ Javascript/fundamental.js
@@ -0,0 +1,4 @@
+console.log("hello index.js");
+let city="Bangalore";
+city="Delhi";
+console.log("city",city);
---
+++ Javascript/index.js
@@ -0,0 +1,46 @@
+// console.log("hello index.js");
+// console.log("hello index.js");
+// let city="Bangalore";
+// city="Delhi";
+// console.log("city",city);
+
+// const country="India";
+// console.log("country",country);
+// let marks = 90;
+// let student = "Rahul";
+// let isPassed = true;
+// let x;
+// let y=null;
+// console.log("marks,student,isPassed,x,y---->",marks,student,isPassed,x,y);
+
+
+// let student="Rahul";
+// let x=5;
+// let z=typeof x;
+// typeof student;
+// console.log("z--->",z);
+
+// let a=10;
+// let b=3.5;
+
+// let inf=Infinity;
+// let notANumber = NaN;
+// console.log(typeof notANumber);
+
+// let age="20";
+// Number(age);
+// parseInt("10.5");
+// console.log("age--->",typeof Number(age),typeof(parseInt("10.5")));
+let firstName= "Aniket";
+let course= "JavaScript";
+
+firstName.length
+firstName.toUpperCase();
+firstName.toLowerCase();
+firstName.includes("Ani");
+
+console.log(firstName.includes("java"));
+let first = "Aniket";
+let last= "Tiwari";
+let full=first+ " " + last;
+console.log(full);
