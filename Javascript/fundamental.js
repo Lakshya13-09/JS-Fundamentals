8e8a7622a70f2242e852ffce8266a5333b72e110
Lakshya13-09 <lakshyajha1309@gmail.com>
2026-03-25 12:06:31 +0530

    new code

--- Javascript/fundamental.js
+++ Javascript/fundamental.js
@@ -2,3 +2,63 @@ console.log("hello index.js");
 let city="Bangalore";
 city="Delhi";
 console.log("city",city);
+let arr=["A","B","C"];
+let a1=[1,2,3];
+let c= [...a1];
+c.push(90);
+console.log(a1);
+console.log(c);
+let d= structuredClone(a1);
+d.push(90);
+console.log(d);
+
+function greet(name , sayBye){
+    console.log(" Hello " + name);
+    sayBye();
+    console.log("call rturning back");
+}
+
+// function sayBye(){
+    
+// } 
+greet("Lakshya" , ()=>{
+    console.log(" say byee");
+});
+
+let result = greet("Lakshya ", ()=>{
+    console.log("Bye !");
+})
+function calculate(a,b,operation){
+    return operation(a,b);
+}
+function add(x,y){
+    return x+y;
+}
+console.log(calculate(10,5,add));
+
+// Every callback is passed to a HOF
+// Function using another function = High Order Function
+// Function being passed = Callback
+
+let students = ["Aniket" , "Lakshya" , "Ayush"];
+students.forEach((name, index) => {
+    console.log(index + "->" + name);
+});
+students.forEach((i,k,j)=>{
+    console.log("array log" , i,k,j);
+});
+// forEach does not return a new array it is just udes to iterate the loop;
+
+let prices = [100,200,300];
+let gstPrices = prices.map(p => p+p*0.18);
+
+let gstPrices2 = prices.map((value)=>{
+    return value + value * 0.18;
+})
+console.log(gstPrices,prices);
+let r1 = prices.map(
+    (x)=>{
+        return x*x;
+    }
+)
+console.log("Map result" ,r1);
