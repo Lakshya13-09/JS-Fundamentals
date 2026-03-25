b04931c037103b37153b061de1abd7163c87c929
Lakshya13-09 <lakshyajha1309@gmail.com>
2026-03-25 14:59:38 +0530

    fundamnetals,dom,js

---
+++ Javascript/dom-model.js
@@ -0,0 +1,13 @@
+// console.dir(document);
+let h=document.getElementById("heading");
+console.log(h);
+let items = document.getElementsByClassName("text");
+console.log(items[0].innerHTML);
+console.log(items);
+
+//query selector
+let item = document.querySelector(".text");
+let item2 = document.querySelectorAll(".text");
+console.log(item);
+console.log(item2);
+
--- Javascript/fundamental.js
+++ Javascript/fundamental.js
@@ -1,64 +1,77 @@
-console.log("hello index.js");
-let city="Bangalore";
-city="Delhi";
-console.log("city",city);
-let arr=["A","B","C"];
-let a1=[1,2,3];
-let c= [...a1];
-c.push(90);
-console.log(a1);
-console.log(c);
-let d= structuredClone(a1);
-d.push(90);
-console.log(d);
+// console.log("hello index.js");
+// let city="Bangalore";
+// city="Delhi";
+// console.log("city",city);
+// let arr=["A","B","C"];
+// let a1=[1,2,3];
+// let c= [...a1];
+// c.push(90);
+// console.log(a1);
+// console.log(c);
+// let d= structuredClone(a1);
+// d.push(90);
+// console.log(d);
 
-function greet(name , sayBye){
-    console.log(" Hello " + name);
-    sayBye();
-    console.log("call rturning back");
-}
+// function greet(name , sayBye){
+//     console.log(" Hello " + name);
+//     sayBye();
+//     console.log("call rturning back");
+// }
 
-// function sayBye(){
+// // function sayBye(){
     
-// } 
-greet("Lakshya" , ()=>{
-    console.log(" say byee");
-});
+// // } 
+// greet("Lakshya" , ()=>{
+//     console.log(" say byee");
+// });
 
-let result = greet("Lakshya ", ()=>{
-    console.log("Bye !");
-})
-function calculate(a,b,operation){
-    return operation(a,b);
-}
-function add(x,y){
-    return x+y;
-}
-console.log(calculate(10,5,add));
+// let result = greet("Lakshya ", ()=>{
+//     console.log("Bye !");
+// })
+// function calculate(a,b,operation){
+//     return operation(a,b);
+// }
+// function add(x,y){
+//     return x+y;
+// }
+// console.log(calculate(10,5,add));
 
-// Every callback is passed to a HOF
-// Function using another function = High Order Function
-// Function being passed = Callback
+// // Every callback is passed to a HOF
+// // Function using another function = High Order Function
+// // Function being passed = Callback
 
-let students = ["Aniket" , "Lakshya" , "Ayush"];
-students.forEach((name, index) => {
-    console.log(index + "->" + name);
-});
-students.forEach((i,k,j)=>{
-    console.log("array log" , i,k,j);
-});
-// forEach does not return a new array it is just udes to iterate the loop;
+// let students = ["Aniket" , "Lakshya" , "Ayush"];
+// students.forEach((name, index) => {
+//     console.log(index + "->" + name);
+// });
+// students.forEach((i,k,j)=>{
+//     console.log("array log" , i,k,j);
+// });
+// // forEach does not return a new array it is just udes to iterate the loop;
 
-let prices = [100,200,300];
-let gstPrices = prices.map(p => p+p*0.18);
+// let prices = [100,200,300];
+// let gstPrices = prices.map(p => p+p*0.18);
 
-let gstPrices2 = prices.map((value)=>{
-    return value + value * 0.18;
-})
-console.log(gstPrices,prices);
-let r1 = prices.map(
-    (x)=>{
-        return x*x;
-    }
-)
-console.log("Map result" ,r1);
+// let gstPrices2 = prices.map((value)=>{
+//     return value + value * 0.18;
+// })
+// console.log(gstPrices,prices);
+// let r1 = prices.map(
+//     (x)=>{
+//         return x*x;
+//     }
+// )
+// console.log("Map result" ,r1);
+let cart=[
+    {item: "Book", price: 200, loss: 50},
+    {item: "Pen", price: 50, loss:90},
+    {item: "Bag", price: 1000,loss:50}
+];
+let initialloss=10;
+let total = cart.reduce((sum,obj)=> sum+obj.price,0);// here 0 is accumulator and this denotes that we have initialized sum with 0;
+let loss= cart.reduce((loss,obj)=>{
+    return loss+obj.loss;
+},initialloss);
+console.log(total);
+console.log(loss);
+
