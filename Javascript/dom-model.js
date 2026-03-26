d917d236c4d142c9f90d0c5c38d6870c5310ee6e
Lakshya13-09 <lakshyajha1309@gmail.com>
2026-03-26 14:11:55 +0530

    New Event JS

--- Javascript/dom-model.js
+++ Javascript/dom-model.js
@@ -11,3 +11,13 @@ let item2 = document.querySelectorAll(".text");
 console.log(item);
 console.log(item2);
 
+let box= document.getElementById("box");
+console.log(box.innerText);// it only prints the valuable information given to print.
+console.log(box.innerHTML);// it return everything written isnisde the html
+box.style.color="red";
+box.style.backgroundColor="yellow";
+box.style.fontSize = "24px";
+
+let newP= document.createElement("p");
+newP.innerText = "I am created by JS";
+document.body.appendChild(newP);//. by this we have created html page by using java scriptg
